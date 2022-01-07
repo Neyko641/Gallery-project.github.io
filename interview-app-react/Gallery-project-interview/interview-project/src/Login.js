85e24f2638626618751a4a712aa8e2811081794b
Neyko <shendamere@gmail.com>
2022-01-07 20:44:17 +0200

    fixed some typos and cleaned up a few other code snippets

--- interview-app-react/Gallery-project-interview/interview-project/src/Login.js
+++ interview-app-react/Gallery-project-interview/interview-project/src/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
                     onSuccess={onLoginSuccess}
                     onFailure={onLoginFailure}
                     cookiePolicy={'single_host_origin'}
-                    isSignedIn={true}
+                  
                 /> 
     );
 }
